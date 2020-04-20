52e45bb1a727af8c0d75ef6697fde13e6595e97a
Damian Ugalde <damianugalde@gmail.com>
2020-04-20 01:57:52 -0700

    Create Decimal Class for Java
    Initial instance variable setup, with default constructor.

---
+++ Decimal.java
@@ -0,0 +1,34 @@
+/**
+ * @author Damian Ugalde
+ * @date 2020-05-07
+ * @version 1.0
+ *
+ * Representing Decimal Numbers
+ * CS 4080 - Concepts of Programming Languages
+ * California State Polytechnic University, Pomona
+ * Computer Science Department
+ *
+ * Instructor: Rahul Talari
+ *
+ */
+public class Decimal {
+
+    private boolean[] dec;
+    private boolean sign;
+    private boolean[] flag;
+
+    public Decimal(){
+        dec = new boolean[96];
+        sign = false;
+        flag = new boolean[8];
+    }
+
+
+    public Decimal(long val){
+
+        //TODO Long conversion constructor
+
+    }
+
+
+}
